fd4c586f174900c516868c11541f1f16010eca9b
guilherme Chiquito <guilherme.fidelis2570@gmail.com>
2026-03-24 16:35:49 -0300

    adiciona model

---
+++ src/main/java/com/example/Produto/models/ProdutoModel.java
@@ -0,0 +1,55 @@
+package com.example.Produto.models;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+
+public class ProdutoModel {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    @Column(name = "valorItem")
+    private BigDecimal preco;
+    @Column(name = "numeroEstoque")
+    private Integer estoque;
+
+    public ProdutoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
